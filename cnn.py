8efb43ec0696ff788d54de1af19af14ccb5a0e60
Jakub Pliszka <jakubpliszka@outlook.com>
2022-06-25 23:04:14 +0200

    Rearranged the cnn file

--- cnn.py
+++ cnn.py
@@ -11,73 +11,101 @@ IMG_WIDTH: int = 128
 IMG_DEPTH: int = 3
 EPOCHS: int = 40
 
+NUMBER_OF_CLASSES: int = 100
 class_names: list = []
 
 
-def build_cnn() -> tf.keras.Sequential:
+def build_and_train_model() -> tf.keras.Sequential:
     """
-    Creates dataset, builds and trains the model, visualizes the results of the training.
-    :return: None
+
+    :return:
     """
     global class_names
 
     train_dataset = tf.keras.utils.image_dataset_from_directory(
         os.path.join(DATASET_PATH, 'train'),
-        seed=123,
+        shuffle=True,
         image_size=(IMG_HEIGHT, IMG_WIDTH),
         batch_size=BATCH_SIZE)
 
     validation_dataset = tf.keras.utils.image_dataset_from_directory(
         os.path.join(DATASET_PATH, 'validate'),
-        seed=123,
+        shuffle=True,
         image_size=(IMG_HEIGHT, IMG_WIDTH),
         batch_size=BATCH_SIZE)
 
     class_names = train_dataset.class_names
-    number_of_classes = len(class_names)
 
     autotune = tf.data.AUTOTUNE
     train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size=autotune)
     validation_dataset = validation_dataset.cache().prefetch(buffer_size=autotune)
 
+    # Build model
+    cnn = build_cnn()
+    cnn.summary()
+
+    # Train model
+    results = cnn.fit(train_dataset, validation_data=validation_dataset, epochs=EPOCHS)
+
+    # Visualize the training results
+    visualize_training_results(results)
+
+
+def build_cnn() -> tf.keras.Sequential:
+    """
+    Creates the CNN architecture.
+    :return: PCompiled model of the CNN.
+    """
+    model = tf.keras.Sequential()
+
+    model.add(tf.keras.layers.Rescaling(1. / 255, input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH)))
+    model.add(data_augmentation())
+
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(2))
+
+    model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(2))
+
+    model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(2))
+
+    model.add(tf.keras.layers.Dropout(0.4))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(units=256, activation='relu'))
+    model.add(tf.keras.layers.Dense(units=NUMBER_OF_CLASSES))
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+
+    return model
+
+
+def data_augmentation() -> tf.keras.Sequential:
+    """
+
+    :return:
+    """
     data_augmentation_layer = tf.keras.Sequential()
     data_augmentation_layer.add(tf.keras.layers.RandomFlip('horizontal',
                                                            input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH)))
     data_augmentation_layer.add(tf.keras.layers.RandomRotation(0.1))
     data_augmentation_layer.add(tf.keras.layers.RandomZoom(0.1))
 
-    cnn = tf.keras.Sequential()
-    cnn.add(tf.keras.layers.Rescaling(1. / 255, input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH)))
-    cnn.add(data_augmentation_layer)
+    return data_augmentation_layer
 
-    cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'))
-    cnn.add(tf.keras.layers.MaxPooling2D(2))
 
-    cnn.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'))
-    cnn.add(tf.keras.layers.MaxPooling2D(2))
+def visualize_training_results(results: tf.keras.callbacks.History) -> None:
+    """
+    Visualize the accuracy and the loss of the training history.
+    :param results: History of the training.
+    :return: None
+    """
+    accuracy = results.history['accuracy']
+    validation_accuracy = results.history['val_accuracy']
 
-    cnn.add(tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'))
-    cnn.add(tf.keras.layers.MaxPooling2D(2))
-
-    cnn.add(tf.keras.layers.Dropout(0.4))
-    cnn.add(tf.keras.layers.Flatten())
-    cnn.add(tf.keras.layers.Dense(units=256, activation='relu'))
-    cnn.add(tf.keras.layers.Dense(units=number_of_classes))
-
-    cnn.compile(optimizer='adam',
-                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                metrics=['accuracy'])
-
-    cnn.summary()
-
-    result = cnn.fit(train_dataset, validation_data=validation_dataset, epochs=EPOCHS)
-
-    # Visualize the training results
-    accuracy = result.history['accuracy']
-    validation_accuracy = result.history['val_accuracy']
-
-    loss = result.history['loss']
-    validation_loss = result.history['val_loss']
+    loss = results.history['loss']
+    validation_loss = results.history['val_loss']
 
     epochs_range = range(EPOCHS)
 
@@ -95,10 +123,14 @@ def build_cnn() -> tf.keras.Sequential:
     plt.title('Training and Validation Loss')
     plt.show()
 
-    return cnn
-
 
 def make_single_prediction(image_path: str, cnn: tf.keras.Sequential) -> None:
+    """
+    Predict the type of the airplane based on the given image.
+    :param image_path: Path to the image to be predicted.
+    :param cnn: Model of the trained CNN.
+    :return: None
+    """
     test_image = tf.keras.utils.load_img(image_path, target_size=(IMG_HEIGHT, IMG_WIDTH))
     image_array = tf.keras.utils.img_to_array(test_image)
     image_array = tf.expand_dims(image_array, 0)  # Create a batch
@@ -108,3 +140,4 @@ def make_single_prediction(image_path: str, cnn: tf.keras.Sequential) -> None:
 
     print(f'This airplane is most likely {class_names[np.argmax(score)]}, '
           f'with a {round(100 * np.max(score))}% confidence.')
+
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os.path
-
-import tensorflow as tf
-
 from split_dataset import split_dataset
-from cnn import build_cnn, make_single_prediction
+from cnn import build_and_train_model, make_single_prediction
 
 
 def main() -> int:
@@ -13,7 +9,7 @@ def main() -> int:
         return 0
 
     print("Dataset split correctly")
-    cnn = build_cnn()
+    cnn = build_and_train_model()
     make_single_prediction("1055151.jpg", cnn)
 
 
